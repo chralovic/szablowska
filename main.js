0c33486bda791bd51813f41c715db5815222e958
khralovich <chralovic@protonmail.com>
2021-06-04 00:58:10 +0200

    add: modal window

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-'use strict';
+'use strict'
+
+
+
+
+const galleryImgArr =  document.querySelectorAll('.content-section__gallery-item');
+const closeBtn = document.querySelector('#close-btn');
+
+
+
+for (let i of galleryImgArr) {
+    i.addEventListener('click', ()=> {
+    i.classList.add("modal");
+    closeBtn.classList.remove("invisible");
+
+    closeBtn.addEventListener('click', ()=> {
+        i.classList.remove("modal");
+        closeBtn.classList.add("invisible");
+    })
+  })
+
+}
+
+
 
