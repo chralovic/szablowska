f20234a0596ae067ca3829877014a87c583094f1
khralovich <chralovic@protonmail.com>
2021-06-05 01:38:33 +0200

    add: pages arrows

--- main.js
+++ main.js
@@ -6,18 +6,27 @@
 const galleryImgArr =  document.querySelectorAll('.content-section__gallery-item');
 const closeBtn = document.querySelector('#close-btn');
 const modalOverlayLayer = document.querySelector('#modal-overlay');
+const leftBtn =  document.querySelector('#btn-left');
+const rightBtn =  document.querySelector('#btn-right');
+
 
 
 for (let i of galleryImgArr) {
     i.addEventListener('click', ()=> {
     i.classList.add("modal");
     closeBtn.classList.remove("invisible");
-    modalOverlayLayer.classList.add("modal-overlay")
+    modalOverlayLayer.classList.add("modal-overlay");
+    rightBtn.classList.remove("invisible");
+    leftBtn.classList.remove("invisible");
+
+
 
     closeBtn.addEventListener('click', ()=> {
         i.classList.remove("modal");
         closeBtn.classList.add("invisible");
         modalOverlayLayer.classList.remove("modal-overlay")
+        rightBtn.classList.add("invisible");
+        leftBtn.classList.add("invisible");
     })
   })
 
