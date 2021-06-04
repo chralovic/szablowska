d697440ac4ad3a7f0a8893f83b01787b240400d4
khralovich <chralovic@protonmail.com>
2021-06-05 01:01:57 +0200

    add: modal window

--- main.js
+++ main.js
@@ -5,17 +5,19 @@
 
 const galleryImgArr =  document.querySelectorAll('.content-section__gallery-item');
 const closeBtn = document.querySelector('#close-btn');
-
+const modalOverlayLayer = document.querySelector('#modal-overlay');
 
 
 for (let i of galleryImgArr) {
     i.addEventListener('click', ()=> {
     i.classList.add("modal");
     closeBtn.classList.remove("invisible");
+    modalOverlayLayer.classList.add("modal-overlay")
 
     closeBtn.addEventListener('click', ()=> {
         i.classList.remove("modal");
         closeBtn.classList.add("invisible");
+        modalOverlayLayer.classList.remove("modal-overlay")
     })
   })
 
