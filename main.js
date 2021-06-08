5752d7caa2d156e068376cd03a036b7c911ed93c
khralovich <chralovic@protonmail.com>
2021-06-09 00:25:22 +0200

    add: chujowa galeria

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const modalOverlayLayer = document.querySelector('#modal-overlay');
 const leftBtn =  document.querySelector('#btn-left');
 const rightBtn =  document.querySelector('#btn-right');
 
-
+/*
 
 for (let i of galleryImgArr) {
     i.addEventListener('click', ()=> {
@@ -19,18 +19,91 @@ for (let i of galleryImgArr) {
     rightBtn.classList.remove("invisible");
     leftBtn.classList.remove("invisible");
 
+   
 
 
     closeBtn.addEventListener('click', ()=> {
-        i.classList.remove("modal");
+      currentImg.classList.remove("modal");
         closeBtn.classList.add("invisible");
         modalOverlayLayer.classList.remove("modal-overlay")
         rightBtn.classList.add("invisible");
         leftBtn.classList.add("invisible");
     })
   })
+}
+*/
 
+
+let counter = 0;
+let currentImg = galleryImgArr[counter];
+
+if (counter == 0) {
+  currentImg.addEventListener('click', ()=> {
+    currentImg.classList.add("modal");
+    closeBtn.classList.remove("invisible");
+    modalOverlayLayer.classList.add("modal-overlay");
+    rightBtn.classList.remove("invisible");
+  })
 }
 
 
+
+
+  rightBtn.addEventListener('click', ()=> {
+    currentImg.classList.remove("modal");
+    currentImg = galleryImgArr[counter+1];
+    if (counter+1 > 0) {
+      leftBtn.classList.remove("invisible");
+    }
+    currentImg.classList.add("modal");
+    closeBtn.classList.remove("invisible");
+    modalOverlayLayer.classList.add("modal-overlay");
+    rightBtn.classList.remove("invisible");
+    return counter = counter+1;
+
+  })
+
+  leftBtn.addEventListener('click', ()=> {
+    currentImg.classList.remove("modal");
+    currentImg = galleryImgArr[counter-1];
+    currentImg.classList.add("modal");
+    closeBtn.classList.remove("invisible");
+    modalOverlayLayer.classList.add("modal-overlay");
+    rightBtn.classList.remove("invisible");
+    return counter = counter-1;
+  })
+
+
+
+
+
+
+
+
+/*
+    galleryImgArr.forEach(img => {
+
+
+
+    
+      img.addEventListener('click', ()=> {
+        img.classList.add("modal");
+        closeBtn.classList.remove("invisible");
+        modalOverlayLayer.classList.add("modal-overlay");
+        rightBtn.classList.remove("invisible");
+        leftBtn.classList.remove("invisible")
+      });
+    })
+*/
+
+
+
+
+    closeBtn.addEventListener('click', ()=> {
+      currentImg.classList.remove("modal");
+        closeBtn.classList.add("invisible");
+        modalOverlayLayer.classList.remove("modal-overlay")
+        rightBtn.classList.add("invisible");
+        leftBtn.classList.add("invisible");
+    })
 
